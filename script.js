59a2e40f407cfa4caeb11e387dd636340b614097
YassinTM <r1114367@student.thomasmore.be>
2025-11-11 14:30:34 +0100

    Found out '' are no backticks but  are. Moved the tasktoAdd to the function itself as it was getting initialised on page load not when the click happens. Using .value instead of .innerText as it is the appropriate tool for input values 

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 // using const so I do not assign other elements to these vars by accident with let.
 
 const addButton = document.getElementById("taskaddbutton");
-//To get not the item but the actual vallue of the input you getElementById and add .innerText
-const taskToAdd = document.getElementById("taskaddinput").innerText;
+//To get not the item but the actual value of the input you getElementById and add .value
+//const taskToAdd = document.getElementById("taskaddinput").value;
 const taskList = document.getElementById("tasklist");
 
 function addTask() {
     //.innerHTML += takes that string and converts it to real HTML elements
-    taskList.innerHTML += '<li>${taskToAdd}</li>';
+  let taskToAdd = document.getElementById("taskaddinput").value;
+    taskList.innerHTML += `<li>${taskToAdd}</li>`;
 }
 //Adding a event listener to the button, this way i can listen to certain actions performed to my button. 
 //Using click event listener here, takes 2 args, type of event, and function to run
