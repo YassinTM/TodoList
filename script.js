0abcd18f7eefeaaa864935747a41f27df52355f6
YassinTM <r1114367@student.thomasmore.be>
2025-11-11 14:24:47 +0100

    Adjuisted the commentary, turned the <li> into a string, edited the addTask fucntion call in the event listener to not have any arguyments, removed parameter of the funciton as it is not needed

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 // using const so I do not assign other elements to these vars by accident with let.
 
 const addButton = document.getElementById("taskaddbutton");
-const taskToAdd = document.getElementById("taskaddinput");
+//To get not the item but the actual vallue of the input you getElementById and add .innerText
+const taskToAdd = document.getElementById("taskaddinput").innerText;
 const taskList = document.getElementById("tasklist");
-function addTask(taskToAdd) {
-    taskList.innerHTML += <li>${taskToAdd}</li>;
+
+function addTask() {
+    //.innerHTML += takes that string and converts it to real HTML elements
+    taskList.innerHTML += "<li>${taskToAdd}</li>";
 }
 //Adding a event listener to the button, this way i can listen to certain actions performed to my button. 
 //Using click event listener here, takes 2 args, type of event, and function to run
+//dont use the addTask() as it calls the function instantly use addTask and let the eventlistener trigger the function itself
 addButton.addEventListener('click', addTask)
